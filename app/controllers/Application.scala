9dc88630227c0fb08a178fbc2f1272df9afc9d31
Muhammet Alay <Muhammet.Alay@monitise.com>
2014-11-07 20:46:17 +0200

    /version is implemented

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,16 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def version = Action {
+    import info.BuildInformation
+    Ok(s"v${BuildInformation.version}, b${BuildInformation.buildDate}, r${BuildInformation.rev}")
   }
 
 }
